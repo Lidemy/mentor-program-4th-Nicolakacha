29f141b88cb2d9f13f806affcd359be56fb0640c
nicolakacha <nicolakacha@gmail.com>
2020-07-01 01:46:53 +0800

    hw1-5完成

--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -10,17 +10,34 @@ rl.on('line', (line) => {
 });
 
 function solve(input) {
-  const arr = input;
-  // eslint-disable-next-line no-unused-vars
-  const number = Number(lines[0]);
-  for (let i = 1; i <= Number(lines[0]); i += 1) {
-    arr[i] = lines[i].split(' ');
+  const arr = [];
+  for (let i = 1; i <= Number(input[0]); i += 1) {
+    arr[i - 1] = input[i].split(' ');
   }
-  console.log(arr);
+  for (let i = 0; i < arr.length; i += 1) {
+    // eslint-disable-next-line no-undef
+    const A = BigInt(arr[i][0]);
+    // eslint-disable-next-line no-undef
+    const B = BigInt(arr[i][1]);
+    // eslint-disable-next-line no-undef
+    const K = Number(arr[i][2]);
+
+    if (A === B) {
+      console.log('DRAW');
+    } else if (A > B && K === 1) {
+      console.log('A');
+    } else if (A < B && K === 1) {
+      console.log('B');
+    } else if (A > B && K === -1) {
+      console.log('B');
+    } else if (A < B && K === -1) {
+      console.log('A');
+    }
+  }
 }
 
 rl.on('close', () => {
   solve(lines);
 });
 
-// solve(["3", "1 2 1", "1 1 -1", "2 2 1"]);
+// solve(["3", "500 300 -1", "1 2 -1", "2 2 1"]);
