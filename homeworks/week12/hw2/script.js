fcef8fbca2208f7199318cb029125429ae88b0e2
nicolakacha <nicolakacha@gmail.com>
2020-09-04 00:10:55 +0800

    week12_homework_completed

--- homeworks/week12/hw2/script.js
+++ homeworks/week12/hw2/script.js
@@ -1,16 +1,17 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable prefer-destructuring */
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-undef */
 
 const APIUrl = 'http://mentor-program.co/mtr04group1/Nicolakacha/week12/todo';
 const template = `
-  <li class="ongoing todo list-group-item d-flex justify-content-between align-items-center">
+  <li class="className todo list-group-item d-flex justify-content-between align-items-center">
     <div class="todo-content">
       <span class="content">xxxxx</span>
       <input type="text" class="d-none">
     </div>
     <div class="functional-btn">
-      <button class="completed btn btn-primary">已完成</button>
+      <button class="check btn btn-primary">已完成</button>
       <button class="edit btn btn-success" data-toggle="modal" data-target="#edit-content">編輯</button>
       <button class="delete btn btn-danger">刪除</button>  
     </div>
@@ -43,10 +44,11 @@ function encodeHTML(s) {
 
 $(document).ready(() => {
   const userID = getUserID();
+
   // add new todo
   $('.submit').click(() => {
     const value = encodeHTML($('.todo-input').val());
-    const newTodo = template.replace(/xxxxx/gi, value);
+    const newTodo = template.replace(/xxxxx/gi, value).replace(/className/gi, 'ongoing');
     $('.todo-input').val('');
     if (value.trim() !== '') {
       $('.reminder').remove();
@@ -55,43 +57,52 @@ $(document).ready(() => {
       $('.main').prepend(reminder);
     }
   });
+
   // delete todo
   $('.todo-list').on('click', '.delete.btn', (e) => {
     $(e.target).parent().parent().remove();
   });
+
   // complete todo
-  $('.todo-list').on('click', '.completed.btn', (e) => {
-    $(e.target)
-      .parent()
-      .parent()
-      .find('.content')
-      .toggleClass('completed-item');
-    $(e.target).parent().parent().toggleClass('completed');
-    $(e.target).parent().parent().toggleClass('ongoing');
-    if ($(e.target).text() === '已完成') {
-      $(e.target).text('未完成');
-      $(e.target).removeClass('btn-primary').addClass('btn-secondary');
-      $(e.target).parent().find('.edit').hide();
+  $('.todo-list').on('click', '.check.btn', (e) => {
+    const btn = $(e.target);
+    const todoForCheck = $(e.target).parent().parent();
+    // 變成沒完成
+    if (btn.hasClass('completed')) {
+      todoForCheck.removeClass('completed');
+      todoForCheck.find('.content').removeClass('completed-item');
+      btn.removeClass('completed');
+      btn.removeClass('btn-secondary').addClass('btn-primary');
+      btn.text('已完成');
+      btn.parent().find('.edit').show();
+    // 變成已完成
     } else {
-      $(e.target).text('已完成');
-      $(e.target).addClass('btn-primary').removeClass('btn-secondary');
-      $(e.target).parent().find('.edit').show();
+      todoForCheck.addClass('completed');
+      todoForCheck.find('.content').addClass('completed-item');
+      btn.addClass('completed');
+      btn.removeClass('btn-primary').addClass('btn-secondary');
+      btn.text('未完成');
+      btn.parent().find('.edit').hide();
     }
   });
+
   // filter to do
   $('.main').on('click', '.all', () => {
     $('.todo').addClass('d-flex').show();
   });
+
   // filter to do
   $('.main').on('click', '.filter-ongoing', () => {
     $('.todo.completed').removeClass('d-flex').hide();
-    $('.todo.ongoing').addClass('d-flex').show();
+    $('.todo:not(.completed)').addClass('d-flex').show();
   });
+
   // filter to do
   $('.main').on('click', '.filter-completed', () => {
-    $('.todo.ongoing').removeClass('d-flex').hide();
+    $('.todo').removeClass('d-flex').hide();
     $('.todo.completed').addClass('d-flex').show();
   });
+
   // edit to do
   $('.main').on('click', '.edit', (e) => {
     const originTodo = $(e.target).parent().parent().find('.content')
@@ -106,8 +117,20 @@ $(document).ready(() => {
 
   // Save todo to database
   $(document).on('click', '.save', () => {
-    const todos = $('.todo-list').html();
-    const newTodos = { userID, todos };
+    const clientTodoList = [];
+    const oldTodos = document.querySelectorAll('.todo');
+    oldTodos.forEach((todo) => {
+      const clientTodo = {};
+      if (todo.classList.contains('completed')) {
+        clientTodo.state = 1;
+      } else {
+        clientTodo.state = 2;
+      }
+      clientTodo.content = todo.querySelector('.content').textContent;
+      clientTodoList.push(clientTodo);
+    });
+    const x = JSON.stringify(clientTodoList);
+    const newTodos = { userID, todos: x };
     $.ajax({
       type: 'POST',
       url: `${APIUrl}/api_add_todos.php`,
@@ -130,8 +153,26 @@ $(document).ready(() => {
       url: `${APIUrl}/api_todos.php?userID=${userID}`,
     })
       .done((data) => {
-        const savedTodo = data.todos[0].todos;
-        $('.todo-list').append(savedTodo);
+        const getTodosFromDatabase = JSON.parse(data.todos[0].todos);
+        getTodosFromDatabase.forEach((storedTodo) => {
+          let li;
+          if (storedTodo.state === 1) {
+            li = template.replace(/xxxxx/gi, storedTodo.content).replace(/className/gi, 'completed');
+          } else {
+            li = template.replace(/xxxxx/gi, storedTodo.content).replace(/className/gi, '');
+          }
+          $('.todo-list').append(li);
+        });
+
+        document.querySelectorAll('.todo').forEach((todo) => {
+          if (todo.classList.contains('completed')) {
+            todo.querySelector('.content').classList.add('completed-item');
+            todo.querySelector('.check.btn').classList.add('completed');
+            todo.querySelector('.check.btn').classList.add('btn-secondary');
+            todo.querySelector('.check.btn').textContent = '未完成';
+            todo.querySelector('.edit').style.display = 'none';
+          }
+        });
       })
       .fail(err => console.log(err));
   }
